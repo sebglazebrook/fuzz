2d1f094d625f0bd0bbbdd0374f8b0b1c48c4e693
Seb Glazebrook <sebastian.glazebrook@redbubble.com>
2016-03-20 23:53:29 +1100

    Ugly but working filter

--- src/fuzz/app.rs
+++ src/fuzz/app.rs
@@ -19,7 +19,7 @@ pub struct App<'a> {
     trans_new_directory_item: Arc<Mutex<Sender<Directory>>>,
     rec_new_directory_item: Arc<Mutex<Receiver<Directory>>>,
     trans_filter_match: Arc<Mutex<Sender<FilteredDirectory<'a>>>>,
-    curses: Arc<Mutex<Curses>>,
+    curses: Curses,
 }
 
 impl<'a> App<'a> {
@@ -36,69 +36,92 @@ impl<'a> App<'a> {
             trans_new_directory_item: Arc::new(Mutex::new(trans_new_directory_item)),
             rec_new_directory_item: Arc::new(Mutex::new(rec_new_directory_item)),
             trans_filter_match: Arc::new(Mutex::new(trans_filter_match)),
-            curses: Arc::new(Mutex::new(Curses::new())),
+            curses: Curses::new(),
         }
     }
 
     pub fn start(&mut self) {
-        self.set_cursor_to_filter_input();
-        self.handle_user_input();
-        self.start_scanning();
-    }
-
-    //---------- private ----------//
-
-    fn start_scanning(&self) {
         let rec_filter_change = self.rec_filter_change.clone();
         let rec_new_directory_item = self.rec_new_directory_item.clone();
         let trans_new_directory_item = self.trans_new_directory_item.clone();
-
         let mut directory = Directory::new(PathBuf::new());
-        crossbeam::scope(|s| {
-            let(trans_new_directory_item, rec_new_directory_item) = channel();
-            let(tx, rx) = channel();
+        let(trans_new_directory_item, rec_new_directory_item) = channel();
+        let(trans_filter_match, rec_filter_match) = channel();
+        //let(tx, rx) = channel();
+        crossbeam::scope(|scope| {
+
             let mut scanner_builder = ScannerBuilder::new();
             scanner_builder = scanner_builder.start_from_path("./");
             scanner_builder = scanner_builder.max_threads(1);
             scanner_builder = scanner_builder.update_subscriber(Arc::new(Mutex::new(trans_new_directory_item)));
             let mut scanner = scanner_builder.build();
             directory = scanner.scan();
-            s.spawn(|| {
-                let(trans_filter_match, rec_filter_match) = channel();
-                let mut filter = ContinuousFilter::new(&directory,
-                                                       //rec_filter_change,
-                                                       Arc::new(Mutex::new(rx)),
-                                                       Arc::new(Mutex::new(rec_new_directory_item)),
-                                                       Arc::new(Mutex::new(trans_filter_match))
-                                                      );
-                crossbeam::scope(|scope| {
-                    scope.spawn(|| {
-                        filter.start();
-                    });
-                });
-                while !self.done.load(Ordering::Relaxed) {
-                    let found = rec_filter_match.recv().unwrap();
-                    println!("found: {}", found.matches.len());
-                }
 
+            let mut filter = ContinuousFilter::new(&directory,
+                                                   rec_filter_change,
+                                                   //Arc::new(Mutex::new(rx)),
+                                                   Arc::new(Mutex::new(rec_new_directory_item)),
+                                                   Arc::new(Mutex::new(trans_filter_match.clone()))
+                                                  );
+
+            scope.spawn(move|| {
+                    filter.start();
             });
+
+            self.set_cursor_to_filter_input();
+
+            while !self.done.load(Ordering::Relaxed) {
+                match self.curses.get_char_and_key() {
+                    Some((character, key)) => {
+                        self.handle_user_input(character, key);
+                    },
+                    None => {
+                        match rec_filter_match.try_recv() {
+                            Ok(filtered_directory) =>  {
+                                self.update_results(filtered_directory);
+                            },
+                            Err(error) => {
+                                match error {
+                                    Empty => {}
+                                    Disconnected => {}
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            self.curses.close();
         });
     }
 
-    fn handle_user_input(&mut self) {
-        //TODO move this to use one tread
-        while !self.done.load(Ordering::Relaxed) {
-            let (character, key) = self.curses.lock().unwrap().get_char_key();
-            if self.is_special_key(&key) {
-                self.handle_special_character(character, &key);
-            } else {
-                self.amend_filter_string(&key);
-            }
+    //---------- private ----------//
+
+    fn handle_user_input(&mut self, character: i32, key: String) {
+        if self.is_special_key(&key) {
+            self.handle_special_character(character, &key);
+        } else {
+            self.amend_filter_string(&key);
         }
-        self.curses.lock().unwrap().close();
     }
 
-    //---------- private -------------//
+    fn update_results(&self, results: FilteredDirectory) {
+        self.clear_results();
+        for (index, result) in results.matches.iter().enumerate() {
+            if index == self.max_result_rows() {
+                break;
+            }
+            self.curses.move_cursor(index as i32, 0);
+            self.curses.println(result);
+        }
+        self.set_cursor_to_filter_input();
+    }
+
+    fn clear_results(&self) {
+        for row in (0..self.max_result_rows()) {
+            self.curses.clear_row(row as i32);
+        }
+    }
 
     fn is_special_key(&self, key: &String) -> bool {
         key.chars().count() != 1
@@ -112,6 +135,7 @@ impl<'a> App<'a> {
                     //KEY_BACKSPACE => {
                     263 => {
                         self.filter_string.pop(); 
+                        self.trans_filter_change.lock().unwrap().send(self.filter_string.clone());
                         self.update_ui();
                     }
                     _ => { }
@@ -122,29 +146,26 @@ impl<'a> App<'a> {
 
     fn amend_filter_string(&mut self, key: &String) {
         self.filter_string = self.filter_string.clone() + key;
-        // TODO send out change event
+        self.trans_filter_change.lock().unwrap().send(self.filter_string.clone());
         self.update_ui();
     }
 
     fn update_ui(&self) {
-        self.set_cursor_to_filter_input();
+        self.set_cursor_to_filter_input_beginning();
         let filter_string = self.filter_string.clone();
-        self.curses.lock().unwrap().println(&filter_string);
+        self.curses.println(&filter_string);
     }
 
     fn set_cursor_to_filter_input(&self) {
-        let curses = self.curses.lock().unwrap();
-        curses.move_cursor(curses.height -1, 0);
+        let column = self.filter_string.chars().count();
+        self.curses.move_cursor(self.curses.height -1, column as i32);
     }
-}
 
-struct StdinHandler {
-    curses: Arc<Mutex<Curses>>,
-}
+    fn set_cursor_to_filter_input_beginning(&self) {
+        self.curses.move_cursor(self.curses.height -1, 0);
+    }
 
-impl StdinHandler {
-
-    pub fn new(curses: Arc<Mutex<Curses>>) -> Self {
-        StdinHandler { curses: curses }
+    fn max_result_rows(&self) -> usize  {
+        (self.curses.height - 1) as usize
     }
 }
--- src/fuzz/curses.rs
+++ src/fuzz/curses.rs
@@ -1,18 +1,24 @@
 use ncurses::*;
+use std::sync::Mutex;
 
 pub struct Curses {
     pub width: i32,
     pub height: i32,
+    //screen: Mutex<*mut i8>,
+    //window: i8,
 }
 
 impl Curses {
 
     pub fn new() -> Self {
-        Curses::init();
+        let window = Curses::init();
+        //set_term(window);
+        //box_(window);
+        //use_window(window);
         let mut width = 0;
         let mut height = 0;
         getmaxyx(stdscr, &mut height, &mut width);  
-        Curses { width: width, height: height  }
+        Curses { width: width, height: height }
     }
 
     pub fn move_cursor(&self, row: i32, column: i32) {
@@ -28,22 +34,37 @@ impl Curses {
         self.print(message);
     }
 
+    pub fn clear_row(&self, row: i32) {
+        self.move_cursor(row, 0);
+        clrtoeol();
+    }
+
     pub fn get_char_key(&self) -> (i32, String) {
         let char = getch();
         (char, keyname(char))
     }
 
+    pub fn get_char_and_key(&self) -> Option<(i32, String)> {
+        nodelay(stdscr, true);
+        let char = getch();
+        if char == -1 {
+            None
+        } else {
+            Some((char, keyname(char)))
+        }
+    }
 
     pub fn close(&self) {
         endwin();
     }
     //--------- private -----------//
 
-    fn init() {
-        initscr();
+    fn init() -> *mut i8 {
+        let window = initscr();
         raw();
         noecho();
         keypad(stdscr, true);
         attron(A_BOLD());
+        window
     }
 }
